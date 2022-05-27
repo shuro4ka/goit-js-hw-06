8a2303b5d12ad815f03d0b5ae73f6ae40e54fc4c
shuro4ka <zzlesia77@gmail.com>
2022-05-27 23:34:09 +0100

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const title = document.querySelector("#ingredients");
+title.textContent = 'Ingredients';
+document.body.append(title);
+console.log(title);
+document.body.appendChild(title);
+
+const listItem = document.createElement("li");
+listItem.classList.add("item");
+listItem.textContent = 'Potatoes';
+console.log(listItem);
+title.appendChild(listItem);
+
+
+
+
+
+
+
+
+// Напиши скрипт, який для кожного елемента 
+// масиву ingredients:
+
+// Створить окремий елемент <li>. 
+// Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його 
+// текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за 
+// одну операцію у список ul.ingredients.
