8e632889b521725128977d63049ad0a120af9ce7
shuro4ka <zzlesia77@gmail.com>
2022-05-29 16:01:23 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,5 @@
 // Лічильник складається зі спану і кнопок, які по кліку 
 // повинні збільшувати і зменшувати його значення на одиницю.
-
 // <div id="counter">
 //   <button type="button" data-action="decrement">-1</button>
 //   <span id="value">0</span>
@@ -12,12 +11,60 @@
 // збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
 
-const counterValue = 0;
 
 
 
-const button = document.querySelector("#counter");
+const counter = document.querySelector("#value");
+let counterValue = 0;
 
-button.addEventListener("click", () => {
-  counterValue ++;
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+
+buttonIncrement.addEventListener("click", () => {
+counterValue += 1;
+counter.textContent = counterValue;
 });
+buttonDecrement.addEventListener("click", () => {
+counterValue -=1;
+counter.textContent = counterValue;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const counter = document.querySelector("#value");
+// let counterValue = 0;
+
+// const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+// const buttonIncrement = document.querySelector('button[data-action="increment"]');
+
+// buttonDecrement.addEventListener("click", () => {
+//   counterValue--;
+//   counter.textContent = counterValue;
+// });
+
+// buttonIncrement.addEventListener("click", () => {
+//   counterValue++;
+//   counter.textContent = counterValue;
+// });
